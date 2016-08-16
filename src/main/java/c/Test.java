b68a0835757c33fc91943e013341b44aab587826
thu <mingfei.net@gmail.com>
2016-08-16 10:55:58 +0800

    Update

--- src/main/java/c/Test.java
+++ src/main/java/c/Test.java
@@ -8,17 +8,37 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  * 2016-08-15 14:56
  */
 public class Test {
+
+    private String string;
+
+    public void setString(String string) {
+        this.string = string;
+    }
+
+    public void hello() {
+        System.out.println("Hello, " + string);
+    }
+
     public static void main(String[] args) {
 //        Business business = new Business(new FloppyWriter());
 //        Business business = new Business(new USBWriter());
 //        business.saveData();
 
         ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
-        Business business = (Business) applicationContext.getBean("business");
-        business.saveData();
+//        Business business = (Business) applicationContext.getBean("business");
+//        business.saveData();
 //        FloppyWriter floppyWriter = (FloppyWriter) applicationContext.getBean("floppyWriter");
 //        floppyWriter.saveToDevice();
 //        USBWriter usbWriter = (USBWriter) applicationContext.getBean("usbWriter");
 //        usbWriter.saveToDevice();
+
+        Test test = (Test) applicationContext.getBean("test");
+//        System.out.println(test);
+        test.hello();
+    }
+
+    @Override
+    public String toString() {return "this is an instance of Test class.";
+
     }
 }
