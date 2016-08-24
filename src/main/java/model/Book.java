48893592fbe7929aa3ff4cd4a982d72bf2cf79a7
thu <mingfei.net@gmail.com>
2016-08-24 13:22:49 +0800

    Update

--- src/main/java/model/Book.java
+++ src/main/java/model/Book.java
@@ -1,44 +1,15 @@
 package model;
 
-import java.io.Serializable;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 /**
  * Created by mingfei.net@gmail.com
  * 2016-08-18 09:16
  */
-public class Book implements Serializable {
+@Data
+@EqualsAndHashCode(callSuper = true)
+public class Book extends BaseModel {
     private Long id;
     private String title;
-
-    public Book() {
-    }
-
-    public Book(Long id, String title) {
-        this.id = id;
-        this.title = title;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    @Override
-    public String toString() {
-        return "Book{" +
-                "id=" + id +
-                ", title='" + title + '\'' +
-                '}';
-    }
 }
--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -1,61 +1,16 @@
 package model;
 
+import lombok.Data;
+
 import java.io.Serializable;
 
 /**
  * Created by mingfei.net@gmail.com
  * 2016-08-17 14:08
  */
+@Data
 public class User implements Serializable {
     private Integer id;
-    private String email, password;
-
-    public User() {
-    }
-
-    public User(String email, String password) {
-        this.email = email;
-        this.password = password;
-    }
-
-    public User(Integer id, String email, String password) {
-
-        this.id = id;
-        this.email = email;
-        this.password = password;
-    }
-
-    public Integer getId() {
-
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    @Override
-    public String toString() {
-        return "User{" +
-                "id=" + id +
-                ", email='" + email + '\'' +
-                ", password='" + password + '\'' +
-                '}';
-    }
+    private String email;
+    private String password;
 }
